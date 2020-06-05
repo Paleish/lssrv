9f12bdf8944303738b24c764d148772a42ecc021
Tan.Yujie <44920181+Paleish@users.noreply.github.com>
2020-06-05 16:56:51 +0800

    lockstep game server demo

---
+++ ls_web_srv/src/main/java/org/bro/lssrv/LssrvApplication.java
@@ -0,0 +1,13 @@
+package org.bro.lssrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LssrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LssrvApplication.class, args);
+    }
+
+}
